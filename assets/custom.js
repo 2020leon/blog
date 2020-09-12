6f7dca7c2ecd53a07a8ec6edc382aff6e79d0479
2020leon <leon2000.tw@gmail.com>
2020-09-12 20:00:10 +0800

    Fix TOC

--- assets/custom.js
+++ assets/custom.js
@@ -1,25 +1,43 @@
-window.addEventListener('DOMContentLoaded', () => {
+(function () {
+	var h2 = document.querySelectorAll('article h2');
+	var h3 = document.querySelectorAll('article h3');
+	var linkList = document.querySelectorAll('#TableOfContents a');
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+	function findLinkElement(name) {
+		for (var i = 0; i < linkList.length; i++) {
+			var items = linkList[i].href.split('#');
+			if (items && items[items.length - 1] === encodeURIComponent(name)) {
+				return i;
 			}
-		});
-	});
+		}
+		return -1;
+	}
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('h2[id],h3[id],h4[id],h5[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
-});
+	function activeLink(titleList) {
+		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		for (var i = titleList.length - 1; i >= 0; i--) {
+			var style = titleList[i].currentStyle || window.getComputedStyle(titleList[i]);
+			if (scrollTop - titleList[i].offsetTop >= -parseFloat(style.marginTop)) {
+				var index = findLinkElement(titleList[i].id);
+				if (index != -1) {
+					var greatGrandparent = linkList[index].parentElement.parentElement.parentElement;
+					if (greatGrandparent.tagName == 'NAV') {
+						linkList[index].parentElement.classList.add('active');
+					}
+					else if (greatGrandparent.classList.contains('active')) {
+						linkList[index].parentElement.classList.add('active');
+					}
+				}
+				break;
+			}
+		}
+	}
 
-function clearActiveStatesInTableOfContents() {
-    document.querySelectorAll('aside nav li').forEach((section) => {
-        section.classList.remove('active');
-    });
-}
+	window.addEventListener("scroll", function () {
+		[].slice.call(linkList).forEach(function (link) {
+			link.parentElement.classList.remove('active');
+		})
+		activeLink(h2);
+		activeLink(h3);
+	})
+})();
