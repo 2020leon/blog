120bcf225c672a380f9908128cbac64296454dfe
2020leon <leon2000.tw@gmail.com>
2020-10-10 15:25:38 +0800

    Edit deploy.sh: fix bug
    Fix bug for the condition of deploying non-markdown files

--- deploy.sh
+++ deploy.sh
@@ -4,29 +4,30 @@ set -e
 echo "\033[0;32mDeploying...\033[0m"
 
 # Update date
-echo "\033[0;34m==>\033[0m Updating date..."
-fileCount=$(git status -s | wc -l)
-if [ $fileCount -gt 1 ]; then
-	echo "\033[1;31mError\033[0m: more than one file are edited or created"
+mdFileCount=$(git status -s | grep -c ".md$" || true)
+if [ mdFileCount -gt 1 ]; then
+	echo "\033[1;31mError\033[0m: more than one .md file are edited or created"
 	exit 1
-fi
-iso8601=$(date +%Y-%m-%dT%H:%M:%S%z)
-iso8601=${iso8601:0:22}:${iso8601:22}
-theFile=$(git status -s)
-fileStatus=${theFile:0:2}
-theFile=${theFile:3}
-dateLine=$(grep $theFile -e "date" -n  | cut -d : -f 1)
-lastmodLine=$(grep $theFile -e "lastmod" -n  | cut -d : -f 1)
+elif [ mdFileCount -eq 1 ]; then
+	echo "\033[0;34m==>\033[0m Updating date..."
+	iso8601=$(date +%Y-%m-%dT%H:%M:%S%z)
+	iso8601=${iso8601:0:22}:${iso8601:22}
+	theFile=$(git status -s | grep ".md$")
+	fileStatus=${theFile:0:2}
+	theFile=${theFile:3}
 
-# Update newest date
-if [ $fileStatus = "??" ]; then
-	sed -i '' -e "$dateLine"'c\
-	date: '"$iso8601" $theFile
-fi
+	# Update newest date
+	if [ $fileStatus = "??" ]; then
+		dateLine=$(grep $theFile -e "date" -n  | cut -d : -f 1)
+		sed -i '' -e "$dateLine"'c\
+		date: '"$iso8601" $theFile
+	fi
 
-# Update newest lastmod
-sed -i '' -e "$lastmodLine"'c\
-lastmod: '"$iso8601" $theFile
+	# Update newest lastmod
+	lastmodLine=$(grep $theFile -e "lastmod" -n  | cut -d : -f 1)
+	sed -i '' -e "$lastmodLine"'c\
+	lastmod: '"$iso8601" $theFile
+fi
 
 # Update themes/axiom
 echo "\033[0;34m==>\033[0m Updating themes/axiom..."
@@ -46,6 +47,8 @@ git add .
 
 # Commit changes.
 echo "\033[0;34m==>\033[0m Commiting changes..."
+iso8601=$(date +%Y-%m-%dT%H:%M:%S%z)
+iso8601=${iso8601:0:22}:${iso8601:22}
 msg="rebuilding site $iso8601"
 if [ -n "$*" ]; then
 	msg="$*"
